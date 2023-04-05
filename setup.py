915c51b8f9102560067d6bd2839dac9a15f1a56b
Anass Yarroudh <72500344+Yarroudh@users.noreply.github.com>
2023-04-05 17:47:29 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
-from setuptools import setup, find_packages()
-    
+from setuptools import setup, find_packages
+
 setup(
     name="optim3d",
     version='0.2.0',
     description="CLI application for efficient and optimized reconstruction of large-scale 3D building models",
     packages=find_packages(),
-    entry_points='''
-        [console_scripts]
-        optim3d=src.optim3d:cli
-    '''
+    entry_points={
+        "console_scripts": [
+            "optim3d=src.optim3d:cli"
+        ]
+    }
 )
