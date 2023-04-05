f92b23adf0e21220fab246ff0473314c172e61ee
Anass Yarroudh <72500344+Yarroudh@users.noreply.github.com>
2023-04-06 01:45:35 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ setup(
     name="optim3d",
     version='0.2.0',
     description="CLI application for efficient and optimized reconstruction of large-scale 3D building models",
+    author = 'Anass Yarroudh',
+    author_email = 'ayarroudh@uliege.be',
+    url = 'https://github.com/Yarroudh/Optim3D',
     packages=find_packages(),
     entry_points={
         "console_scripts": [
