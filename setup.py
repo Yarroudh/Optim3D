932769d5c6023a5ab2f5b2d60639334d86dca824
Anass Yarroudh <72500344+Yarroudh@users.noreply.github.com>
2023-04-05 12:02:34 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,15 @@ try:
 except ImportError:
     from distutils.core import setup
     
+with open('requirements.txt') as file:
+    requirements = file.read().splitlines()
+
 setup(
     name="optim3d",
     version='0.2.0',
     description="CLI application for efficient and optimized reconstruction of large-scale 3D building models",
     packages=["src"]
-    install_requires=reqs
+    install_requires=requirements
     entry_points='''
         [console_scripts]
         optim3d=src.optim3d:cli
