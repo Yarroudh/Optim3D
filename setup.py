fa15df036dea0395baedfc52ada9f51afc3e3ee8
Anass Yarroudh <ayarroudh@uliege.be>
2023-04-06 16:41:27 +0200

    Version 0.1.7

--- setup.py
+++ setup.py
@@ -1,16 +1,24 @@
 from setuptools import setup, find_packages
 
+with open("requirements.txt", "r") as file:
+    requirements = file.read().splitlines()
+
 setup(
     name="optim3d",
-    version='0.1.1',
+    version='0.1.7',
     description="CLI application for efficient and optimized reconstruction of large-scale 3D building models",
     author = 'Anass Yarroudh',
     author_email = 'ayarroudh@uliege.be',
     url = 'https://github.com/Yarroudh/Optim3D',
     packages=find_packages(),
+    py_modules=["optim3d"],
+    install_requires=requirements,
     entry_points={
         "console_scripts": [
             "optim3d=optim3d.main:cli"
         ]
+    },
+     package_data={
+        "optim3d": ["config/*.json"],
     }
 )
