a951176a8223cb289754c5eea0490740c97d3421
Anass Yarroudh <ayarroudh@uliege.be>
2023-04-13 14:13:32 +0200

    Version 0.2.6

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ with open("requirements.txt", "r") as file:
 
 setup(
     name="optim3d",
-    version='0.2.4',
+    version='0.2.6',
     description="CLI application for efficient and optimized reconstruction of large-scale 3D building models.",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    license="BSD 3-Clause BSD 3-Clause \"New\" or \"Revised\" License",
+    license='BSD 3-Clause BSD 3-Clause "New" or "Revised" License',
     author = 'Anass Yarroudh',
     author_email = 'ayarroudh@uliege.be',
     url = 'https://github.com/Yarroudh/Optim3D',
@@ -20,7 +20,7 @@ setup(
             "optim3d=optim3d.main:cli"
         ]
     },
-     package_data={
+    package_data={
         "optim3d": ["config/*.json"],
     }
 )
